e21ce58872b5d7ffd7ad94e5832feb71500d535d
Stephano2 <dazzremie@gmail.com>
2026-06-12 22:45:09 +0300

    installation de dependance

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,16 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          alias: {
-            '@': './src',
-          },
-        },
-      ],
+    presets: [
+      ['babel-preset-expo', {
+        // Désactiver la transformation des imports/exports pour éviter les conflits
+        disableImportExportTransform: true,
+        // Forcer l'utilisation de React JSX runtime automatique
+        jsxRuntime: 'automatic'
+      }]
     ],
+    plugins: [
+      // Pas de plugin reanimated
+    ]
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Ajouter la résolution pour web-streams-polyfill
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  'web-streams-polyfill/ponyfill/es6': require.resolve('web-streams-polyfill/ponyfill/es6')
+};
+
 module.exports = config;
